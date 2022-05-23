ff04e3f6f6ec0c402d8e72dc5ed526b00f17b182
kounelisagis <kounelisagis@gmail.com>
2022-05-24 02:26:44 +0300

    init

---
+++ code.py
@@ -0,0 +1,123 @@
+from machin.frame.algorithms import TD3
+from machin.utils.logging import default_logger as logger
+import torch as t
+import torch.nn as nn
+import numpy as np
+from environment import Continuous_MountainCarEnv
+
+# configurations
+env = Continuous_MountainCarEnv()
+observe_dim = 2
+action_dim = 1
+action_range = 1
+max_episodes = 1000
+max_steps = 200
+solved_reward = -150
+solved_repeat = 5
+
+
+# model definition
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, action_range):
+        super().__init__()
+
+        self.fc1 = nn.Linear(state_dim, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, action_dim)
+        self.action_range = action_range
+
+    def forward(self, state):
+        a = t.relu(self.fc1(state))
+        a = t.relu(self.fc2(a))
+        a = t.tanh(self.fc3(a)) * self.action_range
+        return a
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super().__init__()
+
+        self.fc1 = nn.Linear(state_dim + action_dim, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, 1)
+
+    def forward(self, state, action):
+        state_action = t.cat([state, action], 1)
+        q = t.relu(self.fc1(state_action))
+        q = t.relu(self.fc2(q))
+        q = self.fc3(q)
+        return q
+
+
+if __name__ == "__main__":
+    actor = Actor(observe_dim, action_dim, action_range)
+    actor_t = Actor(observe_dim, action_dim, action_range)
+    critic = Critic(observe_dim, action_dim)
+    critic_t = Critic(observe_dim, action_dim)
+    critic2 = Critic(observe_dim, action_dim)
+    critic2_t = Critic(observe_dim, action_dim)
+
+    td3 = TD3(
+        actor,
+        actor_t,
+        critic,
+        critic_t,
+        critic2,
+        critic2_t,
+        t.optim.Adam,
+        nn.MSELoss(reduction="sum"),
+    )
+
+    episode, step, reward_fulfilled = 0, 0, 0
+    smoothed_total_reward = 0
+
+    while episode < max_episodes:
+        episode += 1
+        total_reward = 0
+        terminal = False
+        step = 0
+        state = t.tensor(env.reset(), dtype=t.float32).view(1, observe_dim)
+        tmp_observations = []
+
+        while not terminal and step <= max_steps:
+            step += 1
+            with t.no_grad():
+                old_state = state
+
+                if step%2 == 1:
+                    action = t.tensor([[np.random.uniform(low=-1, high=1)]])
+                # agent model inference
+                else:
+                    action = td3.act({"state": old_state})
+                # print(action)
+                state, reward, terminal, _ = env.step(action.numpy())
+                state = t.tensor(state, dtype=t.float32).view(1, observe_dim)
+                total_reward += reward
+
+                tmp_observations.append(
+                    {
+                        "state": {"state": old_state},
+                        "action": {"action": action},
+                        "next_state": {"state": state},
+                        "reward": reward,
+                        "terminal": terminal or step == max_steps,
+                    }
+                )
+
+        td3.store_episode(tmp_observations)
+        # update, update more if episode is longer, else less
+        if episode > 100:
+            for _ in range(step):
+                td3.update()
+
+        # show reward
+        smoothed_total_reward = smoothed_total_reward * 0.9 + total_reward * 0.1
+        logger.info(f"Episode {episode} total reward={smoothed_total_reward:.2f}")
+
+        # if smoothed_total_reward > solved_reward:
+        #     reward_fulfilled += 1
+        #     if reward_fulfilled >= solved_repeat:
+        #         logger.info("Environment solved!")
+        #         exit(0)
+        # else:
+        #     reward_fulfilled = 0
---
+++ environment.py
@@ -0,0 +1,63 @@
+import math
+import numpy as np
+from gym import spaces
+
+
+class Continuous_MountainCarEnv():
+
+    def __init__(self):
+        self.min_action = -1.0
+        self.max_action = 1.0
+        self.min_position = -1.2
+        self.max_position = 0.6
+        self.max_speed = 0.07
+        self.goal_position = 0.45
+        self.power = 0.0015
+
+        self.low_state = np.array(
+            [self.min_position, -self.max_speed], dtype=np.float32
+        )
+        self.high_state = np.array(
+            [self.max_position, self.max_speed], dtype=np.float32
+        )
+
+        self.action_space = spaces.Box(
+            low=self.min_action, high=self.max_action, shape=(1,), dtype=np.float32
+        )
+        self.observation_space = spaces.Box(
+            low=self.low_state, high=self.high_state, dtype=np.float32
+        )
+
+    def step(self, action: np.ndarray):
+
+        position = self.state[0]
+        velocity = self.state[1]
+        force = min(max(action[0], self.min_action), self.max_action)
+
+        velocity += force * self.power - 0.0025 * math.cos(3 * position)
+        if velocity > self.max_speed:
+            velocity = self.max_speed
+        if velocity < -self.max_speed:
+            velocity = -self.max_speed
+        position += velocity
+        if position > self.max_position:
+            position = self.max_position
+        if position < self.min_position:
+            position = self.min_position
+        if position == self.min_position and velocity < 0:
+            velocity = 0
+
+        # Convert a possible numpy bool to a Python bool.
+        done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
+
+        reward = 0
+        if done:
+            reward = 100.0
+        reward -= math.pow(action[0], 2) * 0.1
+
+        self.state = np.array([position, velocity], dtype=np.float32)
+        return self.state, reward, done, {}
+
+    def reset(self):
+        self.state = np.array([np.random.uniform(low=-0.6, high=-0.4), 0])
+        return np.array(self.state, dtype=np.float32)
